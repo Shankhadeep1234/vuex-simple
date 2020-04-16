3ae0c358a35802d245d3f0f7a4bd5fea28d4dadd
Shankhadeep1234 <shankhadeepraj@gmail.com>
2020-04-16 22:50:39 +0530

    using actions

--- src/store/store.js
+++ src/store/store.js
@@ -23,4 +23,22 @@ export const store = new Vuex.Store({
       state.counter--;
     },
   },
+  actions: {
+    increment: (context) => {
+      context.commit('increment');
+    },
+    decrement: (context) => {
+      context.commit('decrement');
+    },
+    asyncIncrement: (context) => {
+      setTimeout(() => {
+        context.commit('increment');
+      }, 1000);
+    },
+    asyncDecrement: (context) => {
+      setTimeout(() => {
+        context.commit('decrement');
+      }, 1000);
+    },
+  },
 });
