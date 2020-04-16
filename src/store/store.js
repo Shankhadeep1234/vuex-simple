bc3d95570e48ebd111a9c9d19c09a9b2d809830f
Shankhadeep1234 <shankhadeepraj@gmail.com>
2020-04-16 22:14:49 +0530

    using getters

--- src/store/store.js
+++ src/store/store.js
@@ -7,4 +7,12 @@ export const store = new Vuex.Store({
   state: {
     counter: 0,
   },
+  getters: {
+    doubleCounter: (state) => {
+      return state.counter * 2;
+    },
+    stringCounter: (state) => {
+      return state.counter + 'Click';
+    },
+  },
 });
